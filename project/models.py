77eda0fbad27747b17d0b9b0953b29d7a4688c45
sacred70 <sacred70@mail.ru>
2023-02-28 21:26:12 +0700

    1

--- project/models.py
+++ project/models.py
@@ -1,9 +1,26 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
 from project.setup.db import models
 
-
+"""создаем модель жанра"""
 class Genre(models.Base):
     __tablename__ = 'genres'
 
     name = Column(String(100), unique=True, nullable=False)
+
+"""создаем модель режисера"""
+class Director(models.Base):
+    __tablename__ = 'director'
+
+    name = Column(String(100), unique=True, nullable=False)
+
+
+"""создаем модель фильма"""
+
+
+
+"""создаем модель пользователя"""
+
+
+
+"""создаем модель избранного(не Нео)"""
