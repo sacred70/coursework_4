26d0e7ceac68ee525547cb8f5d7cff63de3070ea
sacred70 <sacred70@mail.ru>
2023-03-01 22:08:07 +0700

    1

--- project/dao/main.py
+++ project/dao/main.py
@@ -1,6 +1,14 @@
 from project.dao.base import BaseDAO
-from project.models import Genre
+from project.models import Genre, Movie, Director
 
 
 class GenresDAO(BaseDAO[Genre]):
     __model__ = Genre
+
+
+class MoviesDAO(BaseDAO[Movie]):
+    __model__ = Movie
+
+
+class DirectorsDAO(BaseDAO[Director]):
+    __model__ = Director
