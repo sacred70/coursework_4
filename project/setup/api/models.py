f3232be6b8d1b12ec230b05e398749f15a6f65fe
sacred70 <sacred70@mail.ru>
2023-03-04 15:23:51 +0700

    api.models

--- project/setup/api/models.py
+++ project/setup/api/models.py
@@ -6,3 +6,29 @@ genre: Model = api.model('Жанр', {
     'id': fields.Integer(required=True, example=1),
     'name': fields.String(required=True, max_length=100, example='Комедия'),
 })
+
+movie_model: Model = api.model('Фильм', {
+    'id': fields.Integer(required=True, example=1),
+    'title': fields.String(required=True, max_length=100, example='Название фильма'),
+    'description': fields.String(max_length=5000, example='Описание фильма'),
+    'trailer': fields.String(max_length=200, example='Фантастика'),
+    'year': fields.Integer(example=2022),
+    'rating': fields.Float(example=4.8),
+    'genre_id': fields.Integer(example=1),
+    'director_id': fields.Integer(example=2),
+    'genre': fields.Nested({'id': fields.Integer, 'name': fields.String}),
+    'director': fields.Nested({'id': fields.Integer, 'name': fields.String})
+})
+
+director_model: Model = api.model('Режиссёр', {
+    'id': fields.Integer(required=True, example=1),
+    'name': fields.String(required=True, max_length=100, example='Джордж Лукас')
+})
+user_model: Model = api.model('Пользователь', {
+    'id': fields.Integer(required=True, example=1),
+    'email': fields.String(required=True, max_length=200, example='user@email.ru'),
+    'password': fields.String(required=True, max_length=200),
+    'name': fields.String(max_length=100),
+    'surname': fields.String(max_length=200),
+    'favourite_genre': fields.Integer(example=1)
+})
